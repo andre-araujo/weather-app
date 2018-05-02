98817b5f9d94a444ac8458be106a4c3447f21608
aluis <andreluis9214@gmail.com>
2018-05-02 20:29:13 -0300

    docs: Update JSdocs of lib fahrenheitToCelcius and range functions

--- src/lib/fahrenheitToCelcius/index.js
+++ src/lib/fahrenheitToCelcius/index.js
@@ -1,3 +1,8 @@
+/**
+ * Converts fahrenheit temperature unit to celcius
+ * @param {String|Number} temperature
+ * @returns {Number}
+ */
 function fahrenheitToCelcius(temperature) {
   if (typeof temperature === 'string' || typeof temperature === 'number') {
     return Math.round((parseFloat(temperature) - 32) / 1.8);
--- src/lib/range/index.js
+++ src/lib/range/index.js
@@ -1,3 +1,12 @@
-export default (start, size) =>
-  Array(...Array(size))
+/**
+ * Creates an array of integers determinated by start and size params
+ * @param {Number} start - start of range
+ * @param {Number} size - count of numbers
+ * @returns {Array}
+ */
+function range(start, size) {
+  return Array(...Array(size))
     .map((_, i) => i + start);
+}
+
+export default range;
